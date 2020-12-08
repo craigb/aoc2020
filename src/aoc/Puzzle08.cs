cdf5b4752999308d6afa2976af34273e9555dddf
Craig Boucher <craig@craigboucher.com>
2020-12-08 09:02:16 -0600

    Add puzzles 7 and 8

---
+++ src/aoc/Puzzle08.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using Xunit;
+using System.IO;
+
+namespace AdventOfCode
+{
+    public class Puzzle08
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Puzzle08(ITestOutputHelper testOutputHelper)
+        {
+            _output = testOutputHelper;
+        }
+
+        public static IEnumerable<object[]> GetInstructions()
+        {
+            yield return new object[] { File.ReadAllLines("Data/08_instructions.txt"), null };
+        }
+
+        public static IEnumerable<object[]> GetExampleInstructions()
+        {
+            yield return new object[]
+            {
+                @"nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6".Split("\r\n"), 5
+            };
+        }
+
+        public static (string Instruction, int Number) ParseInstruction(string instruction)
+        {
+            var split = instruction.Trim().Split(" ");
+            return (split[0], int.Parse(split[1].TrimStart('+')));
+        }
+
+        private static bool TryRunCommands((string Instruction, int Number)[] parsedInstructions, out int acc)
+        {
+            acc = 0;
+            int i = 0;
+            var totalInstructions = parsedInstructions.Length;
+            var visited = Enumerable.Repeat(false, totalInstructions).ToArray();
+
+            while (i < totalInstructions && visited[i] == false)
+            {
+                visited[i] = true;
+                var (add, offset) = parsedInstructions[i] switch
+                {
+                    ("acc", int a) => (a, 1),
+                    ("jmp", int o) => (0, o),
+                    _ => (0, 1)
+                };
+
+                acc += add;
+                i += offset;
+            }
+
+            return i >= totalInstructions;
+        }
+
+        [Theory]
+        [MemberData(nameof(GetExampleInstructions))]
+        [MemberData(nameof(GetInstructions))]
+        public void RunToFirstLoop(string[] instructions, int? accumulator)
+        {
+            var parsedInstructions = instructions.Select(ParseInstruction).ToArray();
+            TryRunCommands(parsedInstructions, out var acc);
+
+            if (accumulator.HasValue)
+            {
+                Assert.Equal(accumulator, acc);
+            }
+            _output.WriteLine($"{acc}");
+        }
+
+        [Theory]
+        [MemberData(nameof(GetExampleInstructions))]
+        [MemberData(nameof(GetInstructions))]
+        public void FindRightMutation(string[] instructions, int? accumulator)
+        {
+            var parsedInstructions = instructions.Select(ParseInstruction).ToArray();
+            var totalInstructions = parsedInstructions.Length;
+            for (int i = 0; i < totalInstructions; ++i)
+            {
+                string tempInstruction = parsedInstructions[i].Instruction;
+                if (tempInstruction == "acc")
+                {
+                    continue;
+                }
+                else if(tempInstruction == "jmp")
+                {
+                    parsedInstructions[i].Instruction = "nop";
+                }
+                else
+                {
+                    parsedInstructions[i].Instruction = "jmp";
+                }
+
+                if (TryRunCommands(parsedInstructions, out int acc))
+                {
+                    _output.WriteLine($"{acc}");
+                    return;
+                }
+
+                parsedInstructions[i].Instruction = tempInstruction;
+            }
+
+        }
+    }
+}
--- src/aoc/Puzzle7.cs
+++ src/aoc/Puzzle7.cs
@@ -3,23 +3,140 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace AdventOfCode.Data
+namespace AdventOfCode
 {
     public class Puzzle7
     {
         private readonly ITestOutputHelper _output;
+
+        private readonly Regex _definitionExtractor = new Regex(@"(?<bagDef>\w+ \w+) bags contain");
+        private readonly Regex _extractor = new Regex(@"bags contain (?:((?<cardinality>\d+) (?<subBag>\w+ \w+) bag[s,]* ?)*)", RegexOptions.Compiled);
+
         public Puzzle7(ITestOutputHelper testOutputHelper)
         {
             _output = testOutputHelper;
         }
 
-        public static IEnumerable<object[]> GetAnswers()
+        public static IEnumerable<object[]> GetBagDefinitions()
         {
-            yield return new[] { File.ReadAllLines(@"Data/") };
+            yield return new[] { File.ReadAllLines(@"Data/07_bags.txt") };
+        }
+
+        public static IEnumerable<object[]> GetKnownBagDefinitions()
+        {
+            yield return new object[]
+            {
+                "posh brown bags contain 5 dim coral bags, 1 plaid blue bag, 2 faded bronze bags, 2 light black bags.",
+                "posh brown",
+                new string[] { "dim coral", "plaid blue", "faded bronze", "light black" },
+                new int[] { 5, 1, 2, 2 }
+            };
+
+            yield return new object[]
+            {
+                "posh brown bags contain no other bags.",
+                "posh brown",
+                new string[] { },
+                new int[] { }
+            };
+        }
+
+        public static IEnumerable<object[]> ToyBagDefinitions()
+        {
+            yield return new[] {
+                @"shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.".Split("\r\n")};
+        }
+
+        private (string, IDictionary<string, int>) DecomposeDefinition(string def)
+        {
+            var defMatch = _definitionExtractor.Match(def);
+            if (!defMatch.Success)
+            {
+                throw new Exception("Unable to parse definition");
+            }
+            var bagDef = defMatch.Groups["bagDef"].Value;
+            var ingredients = new Dictionary<string, int>();
+
+            var m = _extractor.Match(def);
+            if (m.Success)
+            {
+                ingredients = m.Groups["subBag"].Captures.Zip(m.Groups["cardinality"].Captures).ToDictionary(zipped => zipped.First.Value, zipped => int.Parse(zipped.Second.Value));
+            }
+
+            return (bagDef, ingredients);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetKnownBagDefinitions))]
+        public void CanParseBagDefinitions(string definition, string expectedBagDef, string[] bagNames, int[] cardinality)
+        {
+            var (bagDef, ingredients) = DecomposeDefinition(definition);
+            Assert.Equal(expectedBagDef, bagDef);
+            for (int i = 0; i < bagNames.Length; ++i)
+            {
+                Assert.Equal(cardinality[i], ingredients[bagNames[i]]);
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(GetBagDefinitions))]
+        public void CanFindAllShinyGoldBags(string[] definitions)
+        {
+            var seeking = new HashSet<string> { "shiny gold" };
+            var all = definitions.Select(DecomposeDefinition).ToList();
+
+            var startSize = 0;
+            do
+            {
+                startSize = seeking.Count;
+                foreach (var definition in all)
+                {
+                    if (seeking.Any(s => definition.Item2.ContainsKey(s)))
+                    {
+                        seeking.Add(definition.Item1);
+                    }
+                }
+            } while (startSize != seeking.Count);
+
+            _output.WriteLine($"{seeking.Count - 1}");
+        }
+
+        [Theory]
+        [MemberData(nameof(GetBagDefinitions))]
+        [MemberData(nameof(ToyBagDefinitions))]
+        public void CanFindAllBagsInShinyGoldBags(string[] definitions)
+        {
+            var seeking = new Stack<(string, int)>(new[] { ("shiny gold", 1) });
+            var all = definitions.Select(DecomposeDefinition).ToDictionary(elem => elem.Item1, elem => elem.Item2);
+
+            long total = 0;
+
+            while (seeking.Count > 0)
+            {
+                var (current, count) = seeking.Pop();
+                total += count;
+
+                if (all.TryGetValue(current, out var contents) && contents is { Count:>0 })
+                {
+                    foreach (var (def, num) in contents)
+                    {
+                        seeking.Push((def, num * count));
+                    }
+                }
+            }
+
+            _output.WriteLine($"{total - 1}");
         }
     }
 }
